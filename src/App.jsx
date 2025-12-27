a4fa0f0597b04dcc9cdbc116dec2c1122379bca2
19840769 <kodikarah.21@uom.lk>
2025-12-27 16:10:53 +0530

    Add basename to React Router for GitHub Pages subdirectory

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useTheme } from './hooks/useTheme';
-import HomePage from './pages/HomePage';
-import GalleryPage from './pages/GalleryPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useTheme } from "./hooks/useTheme";
+import HomePage from "./pages/HomePage";
+import GalleryPage from "./pages/GalleryPage";
 
 function App() {
   const { isDark, toggleTheme } = useTheme();
 
   return (
-    <Router>
+    <Router basename="/Portfolio-v2">
       <Routes>
-        <Route path="/" element={<HomePage isDark={isDark} toggleTheme={toggleTheme} />} />
-        <Route path="/gallery" element={<GalleryPage isDark={isDark} toggleTheme={toggleTheme} />} />
+        <Route
+          path="/"
+          element={<HomePage isDark={isDark} toggleTheme={toggleTheme} />}
+        />
+        <Route
+          path="/gallery"
+          element={<GalleryPage isDark={isDark} toggleTheme={toggleTheme} />}
+        />
       </Routes>
     </Router>
   );
